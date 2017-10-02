aea9b690a18c522402dc8793c7552c76c2bf58d1
heidilaursen <heidilaursen@protonmail.com>
2017-10-01 20:43:09 -0700

    logos balaned in media small

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 'use strict'
 
-jQuery(function(){
+// jQuery(function(){
   // jQuery('#flipcountdownbox1').flipcountdown({
   //   size: "lg",
   //   showSecond: false
   // });
 
-  $('#countDown').flipcountdown({time:function(){
-		return new Date('11.11.2017');
-	}});
-})
+//   $('#countDown').flipcountdown({time:function(){
+// 		return new Date('11.11.2017');
+// 	}});
+// })
